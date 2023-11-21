74f8588d4bc14f265b992e910a135880f03a4115
BenjaminAndre <andre.vincent.benjamin@gmail.com>
2023-11-22 00:12:17 +0100

    Solutions to day 6

--- day_06a/src/main.rs
+++ day_06a/src/main.rs
@@ -1,3 +1,24 @@
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let data : &[u8; 4096] = include_bytes!("../input.txt");
+    let t0 = Instant::now();
+    let rvt = vt(&data);
+    let dvt = t0.elapsed();
+    println!("vt found {} in {:?}", rvt, dvt);
 }
+
+fn vt(data : &[u8; 4096]) -> u32 {
+    for i in 3..4096 {
+        let byte0 = data[i];
+        let byte1 = data[i-1];
+        let byte2 = data[i-2];
+        let byte3 = data[i-3];
+        if byte0 != byte1 && byte0 != byte2 && byte0 != byte3 && byte1 != byte2 && byte1 != byte3 && byte2 != byte3 {
+            return (i+1).try_into().unwrap();
+        }
+
+    }
+    return 0;
+}
+
--- day_06b/src/main.rs
+++ day_06b/src/main.rs
@@ -1,3 +1,31 @@
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let data : &[u8; 4096] = include_bytes!("../input.txt");
+    let t0 = Instant::now();
+    let rvt = vt(&data);
+    let dvt = t0.elapsed();
+    println!("vt found {} in {:?}", rvt, dvt);
 }
+
+fn vt(data : &[u8; 4096]) -> u32 {
+    const MEMORY: usize = 13;
+    let mut history : [u32;MEMORY] = [0u32;MEMORY];
+    let mut j = 0;
+    for i in 0..4096 {
+        let last_entry = history[(i+MEMORY-1)%MEMORY];
+        let entry = 1u32 << (data[i] - 97);
+        if last_entry ^ entry < last_entry {
+            j = i;
+            history = [0u32;MEMORY];
+            history[i%MEMORY] = entry;
+        } else {
+        if j + MEMORY == i {
+            return (j+MEMORY+1).try_into().unwrap();
+        }
+        history[i%MEMORY] = (last_entry & (!history[i%MEMORY])) | entry;
+        }
+    }
+    return 0;
+}
+
